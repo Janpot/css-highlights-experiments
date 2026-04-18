5d306f5a8b8132a6dbce275a189305b2f3915480
Janpot <2109932+Janpot@users.noreply.github.com>
2026-04-18 06:45:35 +0200

    taghighlighter

--- lib/highlight.ts
+++ lib/highlight.ts
@@ -1,11 +1,39 @@
-import { highlightTree, classHighlighter, Highlighter } from '@lezer/highlight';
+import { highlightTree, tagHighlighter, tags as t, Highlighter } from '@lezer/highlight';
 import { Parser, Tree, TreeFragment, ChangedRange } from '@lezer/common';
 
 export type CompactRange = [string, ...number[]];
 
+export const defaultHighlighter: Highlighter = tagHighlighter([
+  {
+    tag: [
+      t.keyword,
+      t.controlKeyword,
+      t.operatorKeyword,
+      t.definitionKeyword,
+      t.modifier,
+      t.self,
+    ],
+    class: 'lzh-kw',
+  },
+  { tag: [t.string, t.special(t.string)], class: 'lzh-str' },
+  { tag: t.regexp, class: 'lzh-re' },
+  { tag: t.escape, class: 'lzh-esc' },
+  { tag: [t.number, t.integer, t.float], class: 'lzh-num' },
+  { tag: t.bool, class: 'lzh-bool' },
+  { tag: t.null, class: 'lzh-nul' },
+  { tag: [t.lineComment, t.blockComment, t.docComment, t.comment], class: 'lzh-cmt' },
+  { tag: t.variableName, class: 'lzh-var' },
+  { tag: t.propertyName, class: 'lzh-prop' },
+  { tag: t.className, class: 'lzh-cls' },
+  { tag: t.typeName, class: 'lzh-type' },
+  { tag: t.operator, class: 'lzh-op' },
+  { tag: [t.punctuation, t.paren, t.brace, t.bracket, t.separator], class: 'lzh-punct' },
+  { tag: t.meta, class: 'lzh-meta' },
+]);
+
 export function rangesFromTree(
   tree: Tree,
-  highlighter: Highlighter = classHighlighter,
+  highlighter: Highlighter = defaultHighlighter,
 ): CompactRange[] {
   const buckets = new Map<string, number[]>();
   highlightTree(tree, highlighter, (from, to, classes) => {
@@ -16,12 +44,8 @@ export function rangesFromTree(
   return Array.from(buckets, ([token, offsets]) => [token, ...offsets]);
 }
 
-export function computeHighlights(
-  parser: Parser,
-  code: string,
-  highlighter?: Highlighter,
-): CompactRange[] {
-  return rangesFromTree(parser.parse(code), highlighter);
+export function computeHighlights(parser: Parser, code: string): CompactRange[] {
+  return rangesFromTree(parser.parse(code));
 }
 
 export interface ParseState {
